617a98ce07b3f18e59e23d6e4ff36458a9cb0c1f
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-21 11:49:12 -0500

    Update utils.ts

--- src/Input/utils.ts
+++ src/Input/utils.ts
@@ -1,59 +1,75 @@
-import {
-  ICondition,
-  IDecision,
-  IRuleDecision,
-  TypeDataOutput,
-  ValueHowToSetUp,
-} from "./types";
-import * as Yup from "yup";
+import { string, number, StringSchema, NumberSchema, object } from "yup";
+import { ICondition, IDecision, IRuleDecision, TypeDataOutput, ValueHowToSetUp } from "./types";
 
-const typeData = (
-  element: IDecision | ICondition,
-): TypeDataOutput | undefined => {
+const currencyFormat = (price: number): string => {
+  if (price === 0 || !price) {
+    return "$ 0";
+  }
+  return Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+    minimumFractionDigits: 0,
+  }).format(price);
+};
+
+const parseCurrencyString = (currencyString: string): number => {
+  if (currencyString === "$ 0") {
+    return NaN;
+  }
+  return parseInt(currencyString.replace(/\$|\./g, ""));
+};
+
+const parsePercentageString = (percentageString: string): number => {
+  if (percentageString === "0%") {
+    return NaN;
+  }
+  return parseFloat(percentageString.replace("%", ""));
+};
+
+const percentageFormat = (percentage: number): string => {
+  if (percentage === 0 || !percentage) {
+    return "0%";
+  }
+  return `${percentage.toFixed(2)}%`;
+};
+
+const typeData = (element: IDecision | ICondition): TypeDataOutput | undefined => {
   const value = element.possibleValue;
   switch (element.howToSetUp) {
     case ValueHowToSetUp.LIST_OF_VALUES:
       return {
-        schema: Yup.string(),
+        schema: string(),
         value: value.listSelected?.[0],
       };
     case ValueHowToSetUp.LIST_OF_VALUES_MULTI:
       return {
-        schema: Yup.string(),
+        schema: string(),
         value: "",
       };
     case ValueHowToSetUp.RANGE:
       return {
-        schema: Yup.number(),
-        value: {
-          rangeFrom: (value?.rangeFrom as number) || 0,
-          rangeTo: (value?.rangeTo as number) || 0,
-        },
+        schema: number(),
+        value: { rangeFrom: value?.rangeFrom as number || 0, rangeTo: value?.rangeTo as number || 0 },
       };
     case ValueHowToSetUp.GREATER_THAN:
     case ValueHowToSetUp.LESS_THAN:
     case ValueHowToSetUp.EQUAL:
       return {
-        schema: Yup.string().required("Required"),
+        schema: string().required("Required"),
         value: value.value,
       };
     default:
       return {
-        schema: Yup.string(),
+        schema: string(),
         value: undefined,
       };
   }
 };
 
 const ValueValidationSchema = (decision: IRuleDecision) => {
-  const respValue: { [key: string]: Yup.StringSchema | Yup.NumberSchema } = {},
-    initialValues: {
-      [key: string]:
-        | string
-        | number
-        | { rangeFrom: number; rangeTo: number }
-        | undefined;
-    } = {};
+  const respValue: { [key: string]: StringSchema | NumberSchema } = {},
+    initialValues: { [key: string]: string | number | { rangeFrom: number; rangeTo: number } | undefined } = {};
+
   if (decision.decision) {
     const decisionData = typeData(decision.decision);
     if (decisionData) {
@@ -71,26 +87,14 @@ const ValueValidationSchema = (decision: IRuleDecision) => {
       }
     });
   }
-  return { validationSchema: Yup.object().shape(respValue), initialValues };
+  return { validationSchema: object().shape(respValue), initialValues };
 };
 
-export const currencyFormat = (price: number): string => {
-  if (price === 0 || !price) {
-    return "$ 0";
-  }
-  return Intl.NumberFormat("es-CO", {
-    style: "currency",
-    currency: "COP",
-    minimumFractionDigits: 0,
-  }).format(price);
+export {
+  currencyFormat,
+  parseCurrencyString,
+  parsePercentageString,
+  percentageFormat,
+  typeData,
+  ValueValidationSchema,
 };
-
-const parseCurrencyString = (currencyString: string): number => {
-  if (currencyString === "$ 0") {
-    return NaN;
-  }
-
-  return parseInt(currencyString.replace(/\$|\./g, ""));
-};
-
-export { ValueValidationSchema, parseCurrencyString };
